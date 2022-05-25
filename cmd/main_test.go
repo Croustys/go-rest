6e550f9c80ff7c31d9d4801f603c60046d2cc658
Croustys <akos.barabas2002@gmail.com>
2022-05-25 23:15:57 +0200

    added main tests

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,5 +1,27 @@
 package main
 
-//import "test"
+import (
+	"os"
+	"testing"
 
-//tests
+	"github.com/joho/godotenv"
+)
+
+func TestMain(m *testing.M) {
+	exitVal := m.Run()
+	os.Exit(exitVal)
+}
+func TestEnvVars(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		t.Fatal("Error initializing godotenv")
+	}
+
+	MONGO_URI := os.Getenv("MONGO_URI")
+	BCRYPT_SECRET := os.Getenv("BCRYPT_SECRET")
+	JWT_TOKEN := os.Getenv("JWT_TOKEN")
+
+	if MONGO_URI == "" || BCRYPT_SECRET == "" || JWT_TOKEN == "" {
+		t.Fatal("One of the Env vars are missing")
+	}
+}
