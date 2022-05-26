cfd310c6ec515df85028968f4c9e22e766d54b45
Croustys <akos.barabas2002@gmail.com>
2022-05-26 15:34:23 +0200

    google auth

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,112 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
 )
+
+var userTemplate = `
+<p><a href="/logout/{{.Provider}}">logout</a></p>
+<p>Name: {{.Name}} [{{.LastName}}, {{.FirstName}}]</p>
+<p>Email: {{.Email}}</p>
+<p>NickName: {{.NickName}}</p>
+<p>Location: {{.Location}}</p>
+<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></p>
+<p>Description: {{.Description}}</p>
+<p>UserID: {{.UserID}}</p>
+<p>AccessToken: {{.AccessToken}}</p>
+<p>ExpiresAt: {{.ExpiresAt}}</p>
+<p>RefreshToken: {{.RefreshToken}}</p>
+`
+
+func oauthLogin(c *gin.Context) {
+	q := c.Request.URL.Query()
+	q.Add("provider", c.Param("provider"))
+	c.Request.URL.RawQuery = q.Encode()
+
+	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
+	if err != nil {
+		fmt.Fprintln(c.Writer, err)
+		return
+	}
+	t, _ := template.New("foo").Parse(userTemplate)
+	t.Execute(c.Writer, user)
+}
+
+func authProvider(c *gin.Context) {
+	//Building query beacuse gothic doesn't handle gin request
+	q := c.Request.URL.Query()
+	q.Add("provider", c.Param("provider"))
+	c.Request.URL.RawQuery = q.Encode()
+
+	if gothUser, err := gothic.CompleteUserAuth(c.Writer, c.Request); err == nil {
+		t, _ := template.New("foo").Parse(userTemplate)
+		t.Execute(c.Writer, gothUser)
+	} else {
+		gothic.BeginAuthHandler(c.Writer, c.Request)
+	}
+}
+
+func index(c *gin.Context) {
+	m := make(map[string]string)
+	m["google"] = "Google"
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+	t, _ := template.New("foo").Parse(indexTemplate)
+	t.Execute(c.Writer, providerIndex)
+}
+
+func logoutProvider(c *gin.Context) {
+	gothic.Logout(c.Writer, c.Request)
+	c.Writer.Header().Set("Location", "/")
+	c.Writer.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+var indexTemplate = `{{range $key,$value:=.Providers}}
+    <p><a href="/auth/{{$value}}">Log in with {{index $.ProvidersMap $value}}</a></p>
+{{end}}`
 
 func main() {
 	router := gin.Default()
+
+	key := "Secret-session-key" // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30        // 30 days
+	isProd := false             // Set to true when serving over https
+
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New("155971532808-osgvjdl31kht2bv9ifot11rsu99ao116.apps.googleusercontent.com", "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", "http://localhost:3000/auth/google/callback", "email", "profile"),
+	)
+
 	router.POST("/user", createUser)
 	router.POST("/login", auth_middleware, loginUser)
 	router.POST("/findPartner", auth_middleware, findPartner)
+	router.GET("/auth/:provider/callback", oauthLogin)
+	router.GET("/auth/:provider", authProvider)
+	router.GET("/logout/:provider", logoutProvider)
+	router.GET("/", index)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:3000")
+}
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,20 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/joho/godotenv v1.4.0
 	go.mongodb.org/mongo-driver v1.8.4
-	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
+)
+
+require (
+	cloud.google.com/go v0.67.0 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.1.1 // indirect
+	github.com/markbates/goth v1.72.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
 
 require (
@@ -16,7 +29,7 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
@@ -30,8 +43,8 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
